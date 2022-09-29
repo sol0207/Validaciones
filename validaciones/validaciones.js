460f3bac100255d15b588ae9b1c2fac2283f73ed
Sol <solponce0207@gmail.com>
2022-09-28 22:58:13 -0300

    terminado

---
+++ validaciones/validaciones.js
@@ -0,0 +1,31 @@
+export function valida(input){
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[tipoDeInput]){
+        validadores[tipoDeInput](input)
+    }
+}
+
+const validadores ={
+    nacimiento : input => validarNacimiento(input)
+}
+
+function validarNacimiento(input){
+    const fechaCliente = new Date(input.value);
+
+    let mensaje = " ";
+    if(!mayorDeEdad(fechaCliente)){
+        mensaje = "Debes tener al menos 18 años de edad"
+    }
+
+    input.setCustomValidity
+}
+
+function mayorDeEdad (fecha){
+    const fechaActual= new Date();
+    const diferenciaFechas = new Date(fecha.getUTCFullYear()+18,
+    fecha.getUTCMonth(),
+    fecha.getDate()
+    );
+
+    return diferenciaFechas <= fechaActual;
+}
